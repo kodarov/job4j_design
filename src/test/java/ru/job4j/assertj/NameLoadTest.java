4fe9c7c7dafc5d15fe3aadc8407e2e516a113bbb
kodarov_salavat <kodarov@gmail.com>
2024-02-24 04:46:11 +0400

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkNamesEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("array is empty");
+    }
+
+    @Test
+    void checkNamesAssignmentOperator() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "key:value";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("not contain the symbol '='");
+    }
+
+    @Test
+    void checkNamesKey() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "=value";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkNamesValue() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "key=";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a value");
+    }
+}
