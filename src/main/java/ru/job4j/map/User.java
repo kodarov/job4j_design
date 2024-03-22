511abb232726c8e1ca8f66f9dd816ecec7d4e8f3
kodarov_salavat <kodarov@gmail.com>
2024-03-22 04:03:18 +0400

    7. Переопределить и equals и hashCode [#1002 #487210]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class User {
     private String name;
@@ -16,21 +13,51 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (children != user.children) {
+            return false;
+        }
+        if (!Objects.equals(name, user.name)) {
+            return false;
+        }
+        return Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+
     public static void main(String[] args) {
         Map<User, Object> users = new HashMap<>(16);
-        User lexUser = new User("Lex", 3, Calendar.getInstance());
+        Calendar birthday = Calendar.getInstance();
+        User lexUser = new User("Lex", 3, birthday);
         int hashcodeLex = lexUser.hashCode();
         int hashLex = hashcodeLex ^ (hashcodeLex >> 16);
         int bucketLex = hashLex & 15;
         users.put(lexUser, new Object());
 
-        User alexUser = new User("Lex", 3, Calendar.getInstance());
+        User alexUser = new User("Lex", 3, birthday);
         int hashcodeAlex = alexUser.hashCode();
         int hashAlex = hashcodeAlex ^ (hashcodeAlex >> 16);
         int bucketAlex = hashAlex & 15;
         users.put(alexUser, new Object());
-        System.out.printf("hashCode1 %d, hash %d, bucket %d" + System.lineSeparator(), hashcodeLex, hashLex, bucketLex);
-        System.out.printf("hashCode1 %d, hash %d, bucket %d" + System.lineSeparator(), hashcodeAlex, hashAlex, bucketAlex);
+
+        System.out.printf("Lex hashCode %d, hash %d, bucket %d" + System.lineSeparator(), hashcodeLex, hashLex, bucketLex);
+        System.out.printf("Alex hashCode %d, hash %d, bucket %d" + System.lineSeparator(), hashcodeAlex, hashAlex, bucketAlex);
         System.out.println(users);
     }
 }
