e6353436346fe8f9e27ae36b7990f901293f130d
KodarDev <kodarov@gmail.com>
2026-03-09 15:30:10 +0300

    15.1. Регулярные выражения. Метасимволы, квантификаторы, классы символов [#504918 #559628]

--- src/main/java/ru/job4j/regex/RegexExample.java
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -1,5 +1,6 @@
 package ru.job4j.regex;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -73,6 +74,68 @@ public class RegexExample {
     public static void exampleFive() {
         Pattern pattern = Pattern.compile("Job4j");
         String text = "Job4j1 и Job4j2 и Job4j3";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+
+    public static void exampleSix(String[] args) {
+        Pattern pattern = Pattern.compile("11");
+        String text = "111111";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+
+    /**
+     * String.split()
+     *
+     * @param args
+     */
+    public static void exampleSeven(String[] args) {
+        String string = "123+=-456:/789";
+        String[] result = string.split("\\D+");
+        System.out.println(Arrays.toString(result));
+    }
+
+    /**
+     * найти "дд.мм.гггг"
+     *
+     * @param args
+     */
+    public static void exampleEight(String[] args) {
+        Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}\\.\\d{4}");
+        String text = "24.04.1987 11.11.111111 99.99.99991 99.99.9999 99999999 0000.00.00";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+
+    /**
+     * найти "дд.мм.гггг" и ограничением слова по \b
+     *
+     * @param args
+     */
+    public static void exampleNine(String[] args) {
+        Pattern pattern = Pattern.compile("\\b\\d{2}\\.\\d{2}\\.\\d{4}\\b");
+        String text = "24.04.1987 11.11.111111 99.99.99991 99.99.9999 99999999 0000.00.00";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+
+    /**
+     * поиск эл.почты
+     *
+     * @param args
+     */
+    public static void exampleTen(String[] args) {
+        Pattern pattern = Pattern.compile("\\S{1,}@\\S{1,}\\.\\S{1,}");
+        String text = "peter-2022@example.com example65@mail_box.ru 123_45@example-mailbox.com";
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             System.out.println("Найдено совпадение: " + matcher.group());
